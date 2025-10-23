37c1914a12318df342a01e79d0f98bf288363441
aliya mahaboob <mahaboobaliya02@gmail.com>
2025-10-23 16:22:12 +0530

    Dynamically render posts from data source
    Refactored the HTML structure to remove hardcoded post content and added a new data.js file containing post data. Introduced index.js to dynamically render posts into the main section using the data, and updated index.html to load the script as a module. Minor CSS adjustments were made to improve class targeting and font weights.

---
+++ index.js
@@ -0,0 +1,42 @@
+import { posts } from "./data.js";
+
+let postsData = document.querySelector("main");
+
+
+
+function renderPosts() {
+    for (let i = 0; i < posts.length; i++) {
+        postsData.innerHTML += `
+        <div class="post-head">
+          <img
+            src="${posts[i].avatar}"
+            alt="avatar-vangogh"
+            class="avatar"
+          />
+          <div>
+            <h3 class="user-id">${posts[i].name}</h3>
+            <p class="user-location">${posts[i].location}</p>
+          </div>
+        </div>
+        <img
+          src="${posts[i].post}"
+          alt="post-vangogh"
+          class="post-img"
+        />
+        <img src="images/icon-heart.png" alt="heart-icon" class="icon-heart" />
+        <img
+          src="images/icon-comment.png"
+          alt="comment-icon"
+          class="icon-comment"
+        />
+        <img src="images/icon-dm.png" alt="dm-icon" class="icon-dm" />
+        <p class="likes">${posts[i].likes} likes</p>
+        <p class="captions">
+          <span class="user-name">${posts[i].username}</span>
+          ${posts[i].comment}
+        </p>
+         `
+    }
+}
+
+renderPosts();
